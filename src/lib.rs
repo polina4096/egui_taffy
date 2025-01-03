2aa91eb9b85ce7f1d07cb0901c49498dbcedb3ab
Justin C. <636691+boxofrox@users.noreply.github.com>
2025-01-03 06:06:31 -0500

    Set MSRV to 1.81 (#10)
    Update code to remove uses of rust apis that were stabilized after 1.80.
    Additional work needed because dev dependencies depend on MSRV 1.81.

--- src/lib.rs
+++ src/lib.rs
@@ -532,7 +532,7 @@ impl Tui {
         limit: Option<f32>,
         content: impl FnOnce(&mut Ui) -> T,
     ) -> T {
-        let style = params.style.get_or_insert_default();
+        let style = params.style.get_or_insert_with(|| Style::default());
 
         style.overflow = taffy::Point {
             x: taffy::Overflow::Visible,
